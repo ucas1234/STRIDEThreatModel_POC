765d766d5ed45102e4f6cb5ec57763ffb32c10a8
MACKENZIE Lucas <lucas.mackenzie@wavestone.com>
2024-11-19 15:12:37 +0000

    Add_threat function to add threats to out STRIDE threat category object

--- ThreatModel.py
+++ ThreatModel.py
@@ -17,3 +17,29 @@ class STRIDEThreatModel:
             'Elevation of Privilege': [],
         }
     
+    # Create function which will add the details of each threat to the threat category and append risk score
+    def add_threat(self, threat_type, description, component, likelihood, impact, mitigations):
+        # Loop to check if the threat type that has been specified exists in the threats dictionary and create new threat object with all the attributes we need.
+        if threat_type in self.threats:
+            threat = {
+                'description': description,
+                'component': component,
+                'likelihood': likelihood,
+                'impact': impact,
+                'mitigations': mitigations
+            }
+            # Use calculate_risk_score score function to calculate risk score using likelihood * impact
+            risk_score = self.calculate_risk_score(likelihood, impact)
+            # Add this risk score to our threat object as  new string attribute and append to list of threats associated with the specified threat_type
+            threat['risk_score'] = risk_score
+            self.threats[threat_type].append(threat)
+        # Handle error if the specified threat_type is not in the 6 STRIDE threat categories dictionary.
+        else:
+            print("This is not a valid threat type. Please choose a valid threat category from the STRIDE model")
+
+
+    # Create function to calculate the risk score
+    def calculate_risk_score(self, likelihood, impact)
+        
+
+
