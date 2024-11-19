2f5e1cf2e7093ffdcc728ba5a63031a6876336ed
MACKENZIE Lucas <lucas.mackenzie@wavestone.com>
2024-11-19 14:45:19 +0000

    Initialise STRIDE threat categories library

--- ThreatModel.py
+++ ThreatModel.py
@@ -1,1 +1,19 @@
 import json
+
+# Define class STRIDEThreatModel
+class STRIDEThreatModel:
+
+
+    # Create init funtion as constructor of STRIDEThreatModel class. Will be called every time object of class is crrated
+    def __init__(self):
+
+        # Create threats dictionary as an object of class using the 6 STRIDE categories
+        self.threats = {
+            'Spoofing': [],
+            'Tampering': [],
+            'Repudiation': [],
+            'Information Disclosure': [],
+            'Denial of Service': [],
+            'Elevation of Privilege': [],
+        }
+    
